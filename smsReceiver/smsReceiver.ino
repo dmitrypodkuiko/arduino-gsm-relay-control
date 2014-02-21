4a194900f86b3aab60bec816a818f21c2716db77
Dmitry Podkuiko <dpodkuik@gmail.com>
2014-02-20 17:36:31 -0700

    Working relay code and SMS flush fix

--- smsReceiver/smsReceiver.ino
+++ smsReceiver/smsReceiver.ino
@@ -1,6 +1,7 @@
 /*
  This project allows you to control a relay using SMS messaging.
- Required hardware is Arduino UNO/Leonardo, Arduino GSM shield, and a relay.
+ Required hardware is Arduino UNO/Leonardo, Arduino GSM shield, and a relay shield by Evil Mad Science.
+ Relay shield is driven through digital pin 4, which is not used by the GSM shield.
  
  
  Based on GSM examples created
@@ -31,24 +32,20 @@ const String STATUS_COMMAND = "STATUS";
 GSM gsmAccess;
 GSM_SMS sms;
 
-// Array to hold the number a SMS is retreived from
-//char senderNumber[20];
-int relayPin=13;
+int relayPin=4;
 int delayValue = 3000;
-
-
 
 void setup() 
 {
   //set up relay pin
   pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin,LOW);
   
   // initialize serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   } 
-
   Serial.println("SMS Messages Receiver");
     
   // connection state
@@ -77,16 +74,12 @@ void loop()
   // If there are any SMSs available()  
   if (sms.available())
   {
-    Serial.println("Message received from:");
-
-    // An example of message disposal    
-    // Any messages starting with # should be discarded
-    if(sms.peek()=='#')
-    {
-      Serial.println("Discarded SMS");
-      sms.flush();
-    }
+    Serial.println("Message received from:");  
     
+    char senderNumber[20];
+    // Get remote number
+    sms.remoteNumber(senderNumber, 20);
+    Serial.println(senderNumber);
     
     // Read message bytes and print them
     String s = "";
@@ -94,19 +87,15 @@ void loop()
       s = s + c;
       Serial.print(c);
     }
-    
-    char senderNumber[20];
-    // Get remote number
-    sms.remoteNumber(senderNumber, 20);
-    Serial.println(senderNumber);
-    
-    parseSMS(s,senderNumber);  
     Serial.println("\nEND OF MESSAGE");
-    
     // Delete message from modem memory
     sms.flush();
-    s = "";
     Serial.println("MESSAGE DELETED");
+    
+    parseSMS(s,senderNumber);  
+    
+    s = "";
+    
   }
 
   delay(1000);
@@ -141,20 +130,29 @@ void execute(int command, char senderNumber[])
    {
      case TURN_ON:
      {
-       switchOn();
-       sendReply(senderNumber,"");
+       int state = switchOn();
+       char message[20];
+       String stateString = getStateAsString(state);
+       stateString.toCharArray(message,20);
+       sendReply(senderNumber,message);
        break;
      }
      case TURN_OFF:
      {
-       switchOff();
-       sendReply(senderNumber,"");
+       int state = switchOff();
+       char message[20];
+       String stateString = getStateAsString(state);
+       stateString.toCharArray(message,20);
+       sendReply(senderNumber,message);
        break;
      }
      case STATUS:
      {
-       
-       sendReply(senderNumber,"");
+       char message[20];
+       int state = digitalRead(relayPin);
+       String stateString = getStateAsString(state);
+       stateString.toCharArray(message,20);
+       sendReply(senderNumber,message);
        break;
      }
    }
@@ -162,10 +160,7 @@ void execute(int command, char senderNumber[])
 
 void sendReply(char remoteNum[], char textMessage[])
 {
-
-
-  Serial.println(remoteNum);
-    
+  Serial.println(remoteNum);    
   // sms text
   Serial.println("SENDING");
   Serial.println();
@@ -175,26 +170,56 @@ void sendReply(char remoteNum[], char textMessage[])
   // send the message
   sms.beginSMS(remoteNum);
   sms.print(textMessage);
-  sms.endSMS(); 
+  sms.endSMS();
+  sms.flush(); 
   Serial.println("\nCOMPLETE!\n");
 }
 
-
-
-
-void switchOn()
+int switchOn()
 {
-  digitalWrite(relayPin,HIGH);
-  delay(delayValue);
-  digitalWrite(relayPin,LOW);
-  delay(delayValue);
+ digitalWrite(relayPin, HIGH);   
+ int state = digitalRead(relayPin);
+ logState(state);
+ delay(1000);
+ return state;
 }
 
-void switchOff()
+int switchOff()
 {
-  digitalWrite(relayPin,LOW);
-  delay(delayValue);
-  digitalWrite(relayPin,LOW);
-  delay(delayValue);
+  digitalWrite(relayPin, LOW);    // turn the LED off by making the voltage LOW    
+  int state = digitalRead(relayPin);
+  logState(state);          
+  delay(1000);
+  return state;  
 }
+
+void logState(int state)
+{
+ Serial.println(state);
+ if (state == HIGH)
+ {
+   Serial.println("Heat On");
+ }
+ else
+ {
+   Serial.println("Heat Off");
+ }
+}
+
+
+String getStateAsString(int state)
+{
+  String returnArray = String("");
+  if (state == HIGH)
+  {
+    returnArray = "Relay state:ON";
+  }  
+  else
+  {
+    returnArray = "Relay state:OFF";
+  }
+  return returnArray; 
+}
+
+
 
